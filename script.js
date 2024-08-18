3612fde71a92948c5acc1bbf2c72a36b4d599cc9
Alyssa Foote <alyssa.foote.22@gmail.com>
2024-08-18 16:16:39 -0700

    Remove percentage function
    Unnecessary code at the top - redundant

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const divide = (num1, num2) => num1 / num2;
 const multiply = (num1, num2) => num1 * num2;
-const percentage = (num1) => num1 / 100;
 
 // Capture number clicks
 
